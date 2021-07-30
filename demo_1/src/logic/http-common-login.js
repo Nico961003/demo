8c96798d4709fbd001b12ac3a5ec4cdd936faba7
Genaro Rodríguez <genaro.rodriguez@smartqs.com>
2021-07-30 10:46:04 -0500

    test into urls

--- demo_1/src/logic/http-common-login.js
+++ demo_1/src/logic/http-common-login.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 export const HTTP = axios.create({
-  baseURL: `http://localhost:8090/token/`,
+  baseURL: `https://localhost:8090/token/`,
   headers: {
     'Content-Type': 'application/json'
   }
