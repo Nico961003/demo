ecd1952957471eed1b26e5883b36e64591d4078b
Genaro Rodríguez <genaro.rodriguez@smartqs.com>
2021-09-02 12:37:01 -0500

    adding sh to server

---
+++ tasks/test.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+timeout 1000 bash -c '</dev/tcp/127.0.0.1/8090 && echo Puerto abierto || sh ~/keycloak_management/dashboard_back_keycloak/tasks/wake_up_backend.sh ' || echo Timeout de conexión
---
+++ tasks/wake_up_backend.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+#ejecuta una llamada a un jar el cual edita los archivos rechazados por intelar, modifica fecha y secuencial
+# una vez editados son enviados a intelar
+export GRAILS_OPTS="-XX:MaxPermSize=1024m -Xmx1024M -Xms1024m -server"
+echo $GRAILS_OPTS
+rootWFL=$( cd `dirname $0` >/dev/null; pwd );
+
+        EXITCODE=;                                   
+                #funcion que ejecuta el jar
+                RunJava 
+                EXITCODE=$?;                                        
+
+                DISPLAY "El exit code ::--->  $EXITCODE";
+
+              if [ "$EXITCODE" != "0" ]
+                then
+                    DISPLAY "System exit es diferente de 0 , finalizo con error -1  "
+                else            
+                    DISPLAY "Ejecución correcta de jar  ::---> ";
+                    D
+                fi          
+
+# FUNCION PARA EJECUTAR JAVA 
+function RunJava
+{
+echo "Mandando llamar jar con ruta "
+    #ejecuta java, puedes indicar la ruta donde está o pasarla por variable
+    java $DEBUG -jar /root/keycloak_management/dashboard_back_keycloak/build/libs/Project-0.0.1-SNAPSHOT.jar ;
+}   
