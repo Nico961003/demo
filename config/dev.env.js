6bb582c2c03caae3baec829a7213407a929e6a22
Genaro Rodríguez <genaro.rodriguez@smartqs.com>
2021-09-02 10:53:39 -0500

    http

--- config/dev.env.js
+++ config/dev.env.js
@@ -4,7 +4,7 @@ const prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
-  API_ENV: '"https://192.168.1.123:8090/"',
-  TOKEN_ENV: '"https://192.168.1.123:8090/token/"',
+  API_ENV: '"http://192.168.1.123:8090/"',
+  TOKEN_ENV: '"http://192.168.1.123:8090/token/"',
   REALM_ENV: '"DEV"'
 })
