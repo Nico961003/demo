2901a30f29de5907d2776928ddc3fe3b7fc0509b
Genaro Rodríguez <genaro.rodriguez@smartqs.com>
2021-09-11 01:09:07 -0500

    add headers in login petition

--- src/logic/http-common-login.js
+++ src/logic/http-common-login.js
@@ -3,6 +3,11 @@ import axios from 'axios'
 export const HTTP = axios.create({
   baseURL: process.env.TOKEN_ENV,
   headers: {
-    'Content-Type': 'application/json'
+    'Content-Type': 'application/json',
+    'Access-Control-Allow-Credentials': 'true',
+    'Access-Control-Allow-Methods': 'GET, POST, PATCH, DELETE, PUT, OPTIONS',
+    'Access-Control-Allow-Headers': 'Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With',
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Max-Age': 86400
   }
 })
