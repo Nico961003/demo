eb0736421a4607585ef5fd40490f451e9fe5236f
Genaro Rodríguez <genaro.rodriguez@smartqs.com>
2021-11-04 14:19:17 -0600

    client module finished correctly

--- src/views/k_clients/read-Client.vue
+++ src/views/k_clients/read-Client.vue
@@ -9,49 +9,26 @@
       </div>
     </div>
     <!-- Page Title Header Ends-->
-    <div>
       <div class="card pl-4 pt-5 pb-5 pr-4 mt-3">
         <b-container fluid>
-          <!-- Main table element -->
-          <div style="overflow-x:auto;">
-          <table id="tblClients" class="styled-table">
-            <thead>
-              <tr>
-                  <th hidden>Id</th>
-                  <th>#</th>
-                  <th>Nombre</th>
-                  <th>Descripción</th>
-                  <th>Admin URL</th>
-                  <th>baseURL</th>
-                  <th>Operaciones</th>
-               </tr>
-            </thead>
-            <tbody>
-              <tr v-for="(client, index) in clients" :key="index">
-                  <td hidden>{{client.id}}</td>
-                  <td>{{index + 1}}</td>
-                  <td>{{client.clientId}}</td>
-                  <td>{{client.description}}</td>
-                  <td>{{client.adminUrl}}</td>
-                  <td>{{client.baseUrl}}</td>
-                  <td v-if="client.clientId !== 'account' && client.clientId !== 'account-console' && client.clientId !== 'admin-cli' && client.clientId !== 'broker' && client.clientId !== 'realm-management' && client.clientId !== 'security-admin-console'">
-                    <template>
-                      <router-link :to="'/editClient/' + client.clientId">
-                        <img id="modify" src="/static/img/modify.svg" alt="Modify icon" title="Modificar cliente">
-                      </router-link>
-                        <a @click="eliminar(client.clientId)" class="pointer"><img id="delete" src="/static/img/delete_2.svg" alt="Detele icon" title="Eliminar cliente"></a>
-                    </template>
-                  </td>
-                  <td v-else>
-                    Por seguridad, no habilitadas
-                  </td>
-              </tr>
-            </tbody>
-          </table>
-          </div>
+          <v-client-table
+            :data="clients"
+            :options="tableOptions"
+            :columns="tableColumns">
+            <template slot="index" slot-scope="data">{{ data.index }}</template>
+            <template slot="enabled" slot-scope="{row}">
+              <span v-if="row.enabled == true" class="pointer"><center><img id="active" src="/static/img/active.svg" alt="Active icon" title="Usuario Activo"></center></span>
+              <span v-if="row.enabled == false" class="pointer"><center><img id="inactive" src="/static/img/inactive.svg" alt="Inactive icon" title="Usuario Inactivo"></center></span>
+            </template>
+            <span slot="acciones" slot-scope="{row}">
+              <center>
+                <!-- <span @click="modify(row)" class="pointer"><img id="modify" src="/static/img/modify.svg" alt="Modify icon" title="Modificar rol"></span> -->
+                <span @click="eliminar(row)" class="pointer"><img id="delete" src="/static/img/delete_2.svg" alt="Detele icon" title="Eliminar rol"></span>
+              </center>
+            </span>
+          </v-client-table>
         </b-container>
       </div>
-    </div>
   </section>
 </template>
 
@@ -73,10 +50,13 @@ export default {
         // console.log(this.clients)
       })
     },
-    eliminar (clientId) {
+    modify (row) {
+      this.$router.push('/editClient/' + row.id)
+    },
+    eliminar (row) {
       swal({
         title: '¡Advertencia!',
-        text: '¿Eliminar el cliente : ' + clientId + '?',
+        text: '¿Eliminar el cliente : ' + row.name + '?',
         type: 'warning',
         showCancelButton: true,
         confirmButtonColor: '#DD6B55',
@@ -84,7 +64,7 @@ export default {
         cancelButtonText: 'No, cancelar'
       }).then((result) => {
         if (result.value) {
-          this.eliminaRegistro(clientId)
+          this.eliminaRegistro(row.id)
         } else {}
       })
     },
@@ -107,25 +87,59 @@ export default {
   },
   data () {
     return {
-      clients: {}
+      clients: [],
+      formOptions: {
+        validateAfterChanged: true
+      },
+      isSaving: false,
+      rols: [],
+      tableColumns: [
+        'index',
+        'id',
+        'name',
+        'description',
+        'adminUrl',
+        'rootUrl',
+        'enabled',
+        'acciones'
+      ],
+      tableOptions: {
+        headings: {
+          index: '#',
+          id: 'id',
+          name: 'Nombre',
+          description: 'Descripción',
+          adminUrl: 'admin URL',
+          rootUrl: 'root URL',
+          enabled: 'Estatus',
+          acciones: 'Acciones'
+        },
+        filterByColumn: false,
+        texts: {
+          count: 'Mostrando del {from} al {to} de {count} Registros|{count} Registros|1 Registro',
+          first: 'Primero',
+          last: 'Ultimo',
+          filter: false,
+          filterPlaceholder: 'Buscar . . .',
+          limit: 'Mostrar :',
+          page: 'Pagina : ',
+          noResults: 'No se encontraron datos',
+          filterBy: 'Filtro por {column}',
+          loading: 'Cargando...',
+          defaultOption: 'Selecciona {column}',
+          columns: 'Columnas'
+        },
+        hiddenColumns: ['id'],
+        perPage: 5,
+        perPageValues: [5],
+        sortable: ['name', 'description', 'enabled'],
+        filterable: ['name', 'description', 'enabled']
+      }
     }
   },
   mounted () {
     HTTP.get('client/viewClients').then(r => {
       this.clients = r.data
-      // console.log(this.clients)
-      $('#tblClients').DataTable({
-        responsive: true,
-        // scrollY: 540,
-        ordering: true,
-        select: true,
-        'columnDefs': [
-          {'className': 'dt-center', 'targets': '_all'}
-        ],
-        'language': {
-          'url': '//cdn.datatables.net/plug-ins/9dcbecd42ad/i18n/Spanish.json'
-        }
-      })
     })
   },
   computed: {
